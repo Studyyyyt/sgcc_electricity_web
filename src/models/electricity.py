60941d07bcc2f4c67491a636298916349a636b17
Javed <javed_shi@163.com>
2025-01-07 22:10:22 +0800

    fix

--- src/models/electricity.py
+++ src/models/electricity.py
@@ -11,7 +11,6 @@ class Electricity:
         self.is_db_new_create = False
 
         db_path = config.data_path + os.path.sep + self.db_name
-        db_path = self.db_name
         if not os.path.exists(db_path):
             logging.info(f"Database of {db_path} not exists, will created!")
             self.is_db_new_create = True
